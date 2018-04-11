984009e3871326f55e24c70bdb821e2d5d8ed8c6
logicr <13314433445@163.com>
2018-04-11 17:38:39 +0800

    Dynamic SeqList

---
+++ day4-11/test.c
@@ -0,0 +1,13 @@
+#pragma warning(disable:4996)
+# include "SeqListD.h"
+//记得free（）；
+int main()
+{
+	//testInit();
+	//tsetPushBack();
+	//测试任意位置插入删除
+	//testInsert();
+	//测试容量、个数、清空、empty
+	testAll();
+	return 0;
+}
